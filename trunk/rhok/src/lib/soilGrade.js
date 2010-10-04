f34a00182fbeced99ca0936c68c6276c4f375c3d
li.jason.c <li.jason.c@33b91f69-d08c-8e82-7936-3b9adef56591>
2010-10-04 02:40:16 +0000

    * Refactoring form names and ids to simplify processing. * Added deletion of soil strata depth on profile point delete * Added deletion of water depth on profile point delete

--- trunk/rhok/src/lib/soilGrade.js
+++ trunk/rhok/src/lib/soilGrade.js
@@ -1,28 +1,43 @@
 
-function SoilGrade(grade, c, phi, Ksat){
+function SoilGrade(grade, c, phi, ksat) {
   this.grade = grade;
   this.c = c;
   this.phi = phi;
-  this.Ksat = Ksat;
+  this.ksat = ksat;
 }
 
-<!-- Return soil grade constants                   -->
-<!-- Change variables here if it should be changed -->
-function getSoilGrade(num){
-  switch(num){
-    case 1:
-      return new SoilGrade(1, 50, 60, '1e-09');
-    case 2:
-      return new SoilGrade(2, 40, 50, '1e-08');
-    case 3:
-      return new SoilGrade(3, 30, 40, '5e-08');
-    case 4:
-      return new SoilGrade(4, 25, 33, '1e-07');
-    case 5:
-      return new SoilGrade(5, 21, 29, '1e-06');
-    case 6:
-      return new SoilGrade(6, 14, 23, '5e-05');
-    default:
-      return null;
-  }
+/*
+Return soil grade constants
+Change variables here if it should be changed
+*/
+function getSoilGrade(num)
+{
+    if ( '1' === num )
+    {
+        return new SoilGrade(1, 50, 60, '1e-09');
+    }
+    else if ( '2' === num )
+    {
+        return new SoilGrade(2, 40, 50, '1e-08');
+    }
+    else if ( '3' === num )
+    {
+        return new SoilGrade(3, 30, 40, '5e-08');
+    }
+    else if ( '4' === num )
+    {
+        return new SoilGrade(4, 25, 33, '1e-07');
+    }
+    else if ( '5' === num )
+    {
+        return new SoilGrade(5, 21, 29, '1e-06');
+    }
+    else if ( '6' === num )
+    {
+        return new SoilGrade(6, 14, 23, '5e-05');
+    }
+    else
+    {
+        return null;
+    }
 }
