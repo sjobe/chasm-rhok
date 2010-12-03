bd21a73f2afbe9b5dd692430a520e69dc81ad331
li.jason.c <li.jason.c@33b91f69-d08c-8e82-7936-3b9adef56591>
2010-12-03 22:26:06 +0000

    Abstracting output of data to a stream

--- rhok/src/boundaryConditions.php
+++ rhok/src/boundaryConditions.php
@@ -19,19 +19,114 @@
 <?php
 	include_once "parse.php";
 	
+	class Chasm_Boundary_Conditions
+	{
+		const DETENTION_CAPACITY = "0.01";
+		const MAX_SOIL_EVAPORATION = "5e-07";
+		
+		const START_TIME = "0";
+		const STOP_TIME = "359";
+		
+		public static function generateBoundaryConditions( $req, $rainIdx = 0, 
+				$outputStream, $newLine = "\n" )
+		{
+			$upslopeRecharge = $req[ Chasm_Input_Parser::WATER_UPSLOPE_RECHARGE ];
+	
+			// upslope recharge
+			fwrite( $outputStream, $upslopeRecharge . $newLine );
+			
+			// detention capacity / max soil evaporation
+			fwrite( $outputStream, Chasm_Boundary_Conditions::DETENTION_CAPACITY 
+				. " " . Chasm_Boundary_Conditions::MAX_SOIL_EVAPORATION 
+				. $newLine );
+			
+			fwrite( $outputStream, $newLine );
+			
+			// storm start time / stop time
+			fwrite( $outputStream, Chasm_Boundary_Conditions::START_TIME . " " 
+				. Chasm_Boundary_Conditions::STOP_TIME . $newLine );
+			
+			fwrite( $outputStream, $newLine );
+			
+			// loop through 7 days pre-storm
+			for ( $day = 0; $day < 7; $day++ )
+			{
+				$counter = 0;
+				for ( $hour = 0; $hour < 24; $hour++ )
+				{
+					fwrite( $outputStream, "0.0 " );
+					
+					if ( $counter++ > 4 )
+					{
+						fwrite( $outputStream, $newLine );
+						$counter = 0;
+					}
+				}
+				fwrite( $outputStream, $newLine );
+			}
+			
+			// loop through hours
+			$rain = $req[ Chasm_Input_Parser::RAIN ][0];
+			$hours = $rain[ Chasm_Input_Parser::RAIN_DURATION ];
+			$volume = $rain[ Chasm_Input_Parser::RAIN_VOLUME ];
+			
+			// convert volume from mm to m
+			$volume = $volume / 1000;
+			
+			$rainFallPerHour = $volume / $hours;
+			
+			$counter = 0;
+			for ( $hour = 0; $hour < 24; $hour++ )
+			{
+				if ( $hour < $hours )
+				{
+					fwrite( $outputStream, $rainFallPerHour . " " );
+				}
+				else 
+				{
+					fwrite( $outputStream, "0.0 " );
+				}
+				
+				if ( $counter++ > 4 )
+				{
+					fwrite( $outputStream, $newLine );
+					$counter = 0;
+				}
+			}
+			fwrite( $outputStream, $newLine );
+			
+			// loop through 7 days post-storm
+			for ( $day = 0; $day < 7; $day++ )
+			{
+				$counter = 0;
+				for ( $hour = 0; $hour < 24; $hour++ )
+				{
+					fwrite( $outputStream, "0.0 " );
+					
+					if ( $counter++ > 4 )
+					{
+						fwrite( $outputStream, $newLine );
+						$counter = 0;
+					}
+				}
+				fwrite( $outputStream, $newLine );
+			}
+		}
+	}
+	
 	if ( empty($_REQUEST) ) {		
 		$_REQUEST = Chasm_Input_Parser::debugData();
 		echo "<script>alert(\"No data supplied. Using testcase 06 data\");</script>";
 	}  
 
-	$_REQUEST[ "rain" ][0]["duration"] = 3;
 	echo "<div id=\"data\" style=\"display:none\">";
 	print_r($_REQUEST);
 	echo "<hr/>";
 	echo "</div>";	
-
-	$upslopeRecharge = $_REQUEST[ Chasm_Input_Parser::WATER_UPSLOPE_RECHARGE ];
 	
+	Chasm_Boundary_Conditions::generateBoundaryConditions( $_REQUEST, 0, fopen( "php://output", "w" ), "<br/>" );
+	
+/*	$upslopeRecharge = $_REQUEST[ Chasm_Input_Parser::WATER_UPSLOPE_RECHARGE ];
 	
 	// upslope recharge
 	echo $upslopeRecharge . Chasm_Profile_Parser::BR;
@@ -94,7 +189,7 @@
 			echo "0.0 0.0 0.0 0.0 0.0 0.0" . Chasm_Profile_Parser::BR;
 		}	
 		echo Chasm_Profile_Parser::BR;
-	}
+	}*/
 ?>
 </pre>
 </body>
