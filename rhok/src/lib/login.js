04f5b0152e25f4af822aead41ec39be724f25c32
dfygrvty@gmail.com <dfygrvty@gmail.com@33b91f69-d08c-8e82-7936-3b9adef56591>
2011-09-17 15:21:25 +0000

    login would hide user name after success

--- rhok/src/lib/login.js
+++ rhok/src/lib/login.js
@@ -18,9 +18,13 @@ function login(options){
 	$.couch.login({name:user.val(),password:password.val(),
 		success: function(data,text,xhr){
 			loginLogout.val("Logout").closest( options.containerSelector ).effect( options.containerEffect );
-			user.prop("disabled", true).val("");
-			password.prop("disabled",true).val("");
+			user.prop("disabled", true);
+			password.prop("disabled",true);
 			postLogin.trigger(options.postLoginEvent.idEventName, data);
+		},
+		error: function(data,text,xhr){
+			//show something nice
+			loginLogout.click();
 		}
 	});
 }
@@ -28,16 +32,20 @@ function logout(options){
 	options = options || {};
 	$.extend(options,{
 		buttonSelector:"#infoData\\:loginLogout",
+		userSelector:"#infoData\\:user",
+		passwordSelector:"#infoData\\:password",
 		containerSelector:"fieldset",
 		containerEffect:"highlight"
 	});
 	var loginLogout = $(options.buttonSelector);
+	var user = $(options.userSelector);
+	var password = $(options.passwordSelector);
 	$.couch.session(
 		{success:function(data){
 			$.couch.logout(data);
 			loginLogout.val("Login").closest( options.containerSelector ).effect( options.containerEffect );
-			user.prop("disabled", false);
-			password.prop("disabled",false);
+			user.prop("disabled", false).val("");
+			password.prop("disabled",false).val("");
 		}}
 	);
 }
